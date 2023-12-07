297366ddd5ff432e2377b70ba6273e6b36dc0f11
O-01 <sa.an-s@yandex.com>
2023-12-07 10:58:17 -0600

    Corrected render_template with cache_id as UUID4 and corresponding cache_id into jinja within stylesheet links in 0-hbnb.html

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -2,11 +2,13 @@
 """ Starts a Flash Web Application """
 from models import storage
 from models.state import State
-from models.city import City
+# from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
+# from os import environ
 from flask import Flask, render_template
+from uuid import uuid4
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -34,10 +36,13 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places)
+    return render_template(
+        '0-hbnb.html',
+        states=st_ct,
+        amenities=amenities,
+        places=places,
+        cache_id=uuid4()
+    )
 
 
 if __name__ == "__main__":
