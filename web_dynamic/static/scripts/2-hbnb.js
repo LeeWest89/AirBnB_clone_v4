605d56d4085cf8e8fcdbcf281f45f42f9c8f240c
O-01 <sa.an-s@yandex.com>
2023-12-07 23:42:24 -0600

    Added updated files supporting 2-hbnb

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,11 +1,17 @@
 $('document').ready(() => {
   $('input:checkbox:checked').prop('checked', false);
-  
+
   let amenityArray = [];
   $('li input:checkbox').on('change', () => {
     amenityArray = $('li input:checkbox:checked').map((_, obj) => $(obj).data('name')).get();
     // console.log(amenityArray);
     $('.amenities h4').text(amenityArray.join(', '));
     if ($('.amenities h4').is(':empty')) $('.amenities h4').text('\xA0');
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', (dump) => {
+    return dump.status === 'OK'
+      ? $('div#api_status').addClass('available')
+      : $('div#api_status').removeClass('available');
   });
 });
