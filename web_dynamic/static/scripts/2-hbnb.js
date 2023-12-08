5e8ad8c1dc19d2476e83e3a50ebfcc2fb0dcf01d
O-01 <sa.an-s@yandex.com>
2023-12-08 15:36:40 -0600

    Migrated margin-right 10px for checkboxes to 6-filters, modified 2-hbnb.js to accommodate 127.0.0.1 (localhost)

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,6 +12,10 @@ $('document').ready(() => {
   $.get('http://0.0.0.0:5001/api/v1/status/', (dump) => {
     return dump.status === 'OK'
       ? $('div#api_status').addClass('available')
-      : $('div#api_status').removeClass('available');
+      : $.get('http://127.0.0.1:5001/api/v1/status/', (dump) => {
+        return dump.status === 'OK'
+          ? $('div#api_status').addClass('available')
+          : $('div#api_status').removeClass('available');
+      });
   });
 });
