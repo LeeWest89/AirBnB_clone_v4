d76903d036efb95fb899fec396df058699993166
O-01 <sa.an-s@yandex.com>
2023-12-09 12:03:38 -0600

    Improved object gathering based on checked amenity checkboxes

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -3,10 +3,16 @@ const url = `http://${window.location.hostname}`;
 $('document').ready(() => {
   $('input:checkbox:checked').prop('checked', false);
 
+  let amenityThing = [];
   let amenityArray = [];
+  let amenityDict = {};
   $('li input:checkbox').on('change', () => {
+    amenityThing = $('li input:checkbox:checked').map((_, obj) => $(obj).data('id')).get();
     amenityArray = $('li input:checkbox:checked').map((_, obj) => $(obj).data('name')).get();
+    amenityDict = amenityThing.reduce((obj, key, idx) => ({ ...obj, [key]: amenityArray[idx] }), {});
+    // console.log(amenityThing);
     // console.log(amenityArray);
+    console.log(amenityDict);
     $('.amenities h4').text(amenityArray.join(', '));
     if ($('.amenities h4').is(':empty')) $('.amenities h4').text('\xA0');
   });
@@ -15,5 +21,5 @@ $('document').ready(() => {
     return dump.status === 'OK'
       ? $('div#api_status').addClass('available')
       : $('div#api_status').removeClass('available');
-      });
   });
+});
